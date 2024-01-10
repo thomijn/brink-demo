29347e8086bdc39ec94a7bd04fc56077ecaf1963
Thomas Gertenbach <thomas.gertenbach99@gmail.com>
2024-01-10 19:47:37 +0100

    v1

---
+++ src/constants.js
@@ -0,0 +1,53 @@
+export const configOptions = [
+  {
+    id: "Cube_1",
+    label: "null",
+    tagPosition: false,
+  },
+  {
+    id: "Cube_2",
+    label: "glasspanel",
+    tagPosition: [0.3, 1, 0],
+    glassPanel: true,
+  },
+  {
+    id: "Cube_3",
+    label: "panels gaps",
+    tagPosition: false,
+  },
+  {
+    id: "Cube_4",
+    label: "Front panels",
+    tagPosition: [2.2, 0.3, 0],
+  },
+  {
+    id: "Cube_5",
+    label: "Side panels",
+    tagPosition: [0.2, 0.4, 2],
+  },
+  {
+    id: "Cube_6",
+    label: "Back corners",
+    tagPosition: [-1.4, 1, -2.1],
+  },
+  {
+    id: "Cube_7",
+    label: "Back panels",
+    tagPosition: [-2, 1, -0.25],
+  },
+  {
+    id: "Cube_8",
+    label: "Top panel",
+    tagPosition: [-1.8, 2.2, 0],
+  },
+  {
+    id: "Cube_9",
+    label: "Pedestal",
+    tagPosition: [0, -0.3, -2.1],
+  },
+  {
+    id: "Cube_10",
+    label: "Front corners",
+    tagPosition: [2, 0.3, 2],
+  },
+];
